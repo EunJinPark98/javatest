a327afbb25713dbd31cc0dbeae9c2a095d3be884
박은진 <obliviscor29@naver.com>
2023-01-04 17:36:18 +0900

    second

--- java230103/src/java230103/hello01.java
+++ java230103/src/java230103/hello01.java
@@ -4,7 +4,8 @@ public class hello01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("오랜만이다 자바");
+		System.out.println("오랜만이다");
+		System.out.println("자바");
 	}
 
 }
